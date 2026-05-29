1886bfc47a3937914bd84220c9f3a142ab1529d8
citamil <146356349+citamil@users.noreply.github.com>
2026-05-29 21:31:35 +0000

    added Morocco API

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+
+  const [team, setTeam] = useState<any>(null)
+
+  useEffect(() => {
+
+    async function getMoroccoTeam() {
+
+      const response = await fetch(
+        "https://www.thesportsdb.com/api/v1/json/123/lookupteam.php?id=136139"
+      )
+
+      const data = await response.json()
+
+      console.log(data)
+
+      setTeam(data.teams[0])
+    }
+
+    getMoroccoTeam()
+
+  }, [])
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+
+      {team ? (
+        <div>
+
+          <img
+            src={team.strBadge}
+            alt="Morocco Badge"
+            width="200"
+          />
+
+          <h1>{team.strTeam}</h1>
+
+          <p>{team.strCountry}</p>
+
+          <p>{team.strLeague}</p>
+
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+
+    </div>
+  )
+}
+
+export default App
